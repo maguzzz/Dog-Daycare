56314b8c9341b09c734e0ad66af057ea1de790c0
maguzzz <markusm.touray@gmail.com>
2022-11-17 12:29:25 +0100

    Done

--- Simple.py
+++ Simple.py
@@ -1,7 +1,6 @@
 dogBreeds = ["Great Pyrenees", "Australian Shepherd" ,"Golden Retriever","Bulldog"]
 input = input("Which dog breed do you want: ")
 
-
 #Getting every item in dogBreeds checking the input and looking for the same "Name" in the list
 for i in dogBreeds:
     if input == i:
