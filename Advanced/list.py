82370e08a53800d78b77bbf553065e6a62444c20
maguzzz <markusm.touray@gmail.com>
2022-11-21 23:54:25 +0100

    __str__ problem solved

--- Advanced/list.py
+++ Advanced/list.py
@@ -1,4 +1,4 @@
-#Creating Class dog for ease of creatin dogs
+#Creating Class dog for ease of creating dogs
 class Dog:
     
     #Creating constructor and Objects
@@ -7,7 +7,7 @@ class Dog:
         self.name = name
         self.age = age
         self.pickup = pickup
-           
+         
     #Checking if the dog is ready for Pickup
         if self.pickup == True:
             self.pickup = self.name + " is ready for pickup"
@@ -16,12 +16,14 @@ class Dog:
         else:
             self.pickup = "ERROR"
 
-#list of dogs
+    def __str__(self):
+        return f'--------------------------\n{self.breed}\n{self.name}\n{self.age}\n{self.pickup}'
 
+#list of dogs
 dog1 = Dog("Australian Shepherd","Clair","5",True)
 dog2 = Dog("Great Pyrenees","Bond","3",True)
 dog3 = Dog("Shiba Inu","Ubuntu","2",False)
 dog4 = Dog("Golden Retriever","Micheal","8","Test")
 
-
-#Struggling to get string value out of internal memory address
+print(dog2.__str__())
+print(dog1.__str__())
