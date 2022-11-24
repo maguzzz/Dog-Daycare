c809cd1f0be44644e145b8938afacb7c5a063fbf
maguzzz <markusm.touray@gmail.com>
2022-11-24 14:11:28 +0100

    input check and dog print

--- main.py
+++ main.py
@@ -3,5 +3,22 @@ from list import *
 #PROBLEM #1 | i want to get all class items without creating a list first
 allDogs = [dog1,dog2,dog3,dog4]
 
+#checking input value
+while True:
+    try:
+        print("--------------------------------")
+        inputName = str(input("DogName: "))
+        inputAge = int(input("Dog Age: "))
+        print("--------------------------------")
+    except ValueError:
+        print("Invalid input.")
+        continue
+    else:
+        break
+
 for i in allDogs:
-    print(i)
+    if (inputAge == i.age) and (inputName == i.name):
+        print(i)
+        break
+    else:
+            break
