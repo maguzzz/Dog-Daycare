9be92766d15c5e625962db873d3f0f075e6b9b47
maguzzz <markusm.touray@gmail.com>
2022-11-25 23:39:58 +0100

    restart possible

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import os
+import sys
 from list import *
 
 #PROBLEM #1 | i want to get all class items without creating a list first
@@ -6,7 +8,7 @@ allDogs = [dog1,dog2,dog3,dog4]
 #checking input value
 while True:
     try:
-        print("--------------------------------")
+        print("\n--------------------------------")
         inputName = str(input("DogName: "))
         inputAge = int(input("Dog Age: "))
         print("--------------------------------")
@@ -17,8 +19,15 @@ while True:
         break
 
 for i in allDogs:
-    if (inputAge == i.age) and (inputName == i.name):
+    if (inputName == i.name) and (inputAge == i.age):
         print(i)
         break
     else:
+        retry = input("Try again? y/n: ").lower()
+        if retry == "y":
+            os.execl(sys.executable, sys.executable, *sys.argv)
+        elif retry == "n":
+            print("Goodbye")
             break
+        else:
+            print("ERROR")
